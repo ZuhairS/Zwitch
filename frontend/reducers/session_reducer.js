4e1e9b02d3971224457e42f85579ed91e73bfbdf
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-07-18 11:59:18 -0700

    Add session actions and reducers, create store, create root and App view

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+
+const defaultState = () => ({
+  currentUser: null,
+  errors: []
+});
+
+const SessionReducer = (state = defaultState(), action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, state, { currentUser: action.currentUser });
+    case RECEIVE_ERRORS:
+      return Object.assign({}, state, { errors: action.errors });
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
--- frontend/zwitch.jsx
+++ frontend/zwitch.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to the awesomeness that will be Zwitch!</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
 
 window.signup = signup;
