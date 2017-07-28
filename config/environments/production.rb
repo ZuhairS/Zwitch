bb55a5546f944f1002081261cc8895b138313ad4
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-07-27 19:12:35 -0700

    Prep heroku for live chat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #For action cable
+  config.web_socket_server_url = "wss://zwitch.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://zwitch.herokuapp.com', 'http://zwitch.herokuapp.com']
+
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
