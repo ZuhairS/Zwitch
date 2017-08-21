70c5ad7ae123ccdd8409a16392dcee659aa69c54
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-08-20 20:07:13 -0700

    Remove app breaking code from channels

--- frontend/components/channels/channels.jsx
+++ frontend/components/channels/channels.jsx
@@ -3,19 +3,12 @@ import { Link } from 'react-router-dom';
 import { youtubeIdExtractor } from '../../util/video_util';
 
 export default class Channels extends React.Component {
-
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
     this.props.requestChannels();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.match.params.channelId !== nextProps.match.params.channelId) {
-      this.props.requestChannels();
-    }
   }
 
   render() {
@@ -26,37 +19,43 @@ export default class Channels extends React.Component {
     if (channels) {
       return (
         <section className={className}>
-        <h1>{title}</h1>
-         <ul className='flex-grid channels'>
-           {channels.map(channel => (
-            <li key={channel.id} className="channel-item">
-              <Link to={`/channels/${channel.id}`}>
-                <img
-                  src={`https://img.youtube.com/vi/${youtubeIdExtractor(channel.video_url)}/maxresdefault.jpg`}
-                  alt={channel.channel_name}
+          <h1>
+            {title}
+          </h1>
+          <ul className="flex-grid channels">
+            {channels.map(channel =>
+              <li key={channel.id} className="channel-item">
+                <Link to={`/channels/${channel.id}`}>
+                  <img
+                    src={`https://img.youtube.com/vi/${youtubeIdExtractor(
+                      channel.video_url
+                    )}/maxresdefault.jpg`}
+                    alt={channel.channel_name}
                   />
-              </Link>
-
-              <span id='channel-info'>
-                <Link className='link' to={`/channels/${channel.id}`}>
-                  <h4>{ channel.stream_name }</h4>
                 </Link>
-                <p>{Math.floor((Math.random() * 1000) + 1) + ' viewers on '}
-                  <Link className='link' to={`/channels/${channel.id}`}>
-                    <strong>{channel.channel_name}</strong>
+
+                <span id="channel-info">
+                  <Link className="link" to={`/channels/${channel.id}`}>
+                    <h4>
+                      {channel.stream_name}
+                    </h4>
                   </Link>
-                </p>
-              </span>
-            </li>
-          ))}
-         </ul>
-       </section>
+                  <p>
+                    {Math.floor(Math.random() * 1000 + 1) + ' viewers on '}
+                    <Link className="link" to={`/channels/${channel.id}`}>
+                      <strong>
+                        {channel.channel_name}
+                      </strong>
+                    </Link>
+                  </p>
+                </span>
+              </li>
+            )}
+          </ul>
+        </section>
       );
     } else {
-      return (
-        <div>Fetching...</div>
-      );
+      return <div>Fetching...</div>;
     }
   }
-
 }
